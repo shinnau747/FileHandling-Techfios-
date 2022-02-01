6a14bc4bf502ad98069870a2cda647507c425df2
shinnau747 <nasimnaushin@gmail.com>
2022-01-31 21:36:48 -0600

    commiting

---
+++ src/FileInputOutput/FileOperations.java
@@ -0,0 +1,39 @@
+package FileInputOutput;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileOperations {
+
+	public static void main(String[] args) {
+	
+		String filepath = ".\\wordmeaning.txt";
+		doesFileExist(filepath);
+
+	}
+
+	private static void doesFileExist(String path) {
+	
+		try {
+			Scanner scan = new Scanner(new File(path));
+			System.out.println("Path to file exists");
+
+			while (scan.hasNextLine()) {
+				String str=scan.nextLine();
+				String[] res = str.split("[-\\,]",0);
+
+				for (String myStr: res) {
+					System.out.println(myStr.trim());
+			
+				}
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
